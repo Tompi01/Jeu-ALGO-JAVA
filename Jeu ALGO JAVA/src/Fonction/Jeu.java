0677eeb19eef69e7522b92c8e3b4be697a2e4f40
Elwan Beauchot <elwan.beauchot-le-bon@edu.esiee-it.fr>
2024-02-06 23:13:59 +0100

    premier joueur désigné aléatoirement

--- Jeu ALGO JAVA/src/Fonction/Jeu.java
+++ Jeu ALGO JAVA/src/Fonction/Jeu.java
@@ -1,4 +1,5 @@
 package Fonction;
+import java.util.Random;
 
 public class Jeu {
     public static void jeu() {
@@ -12,6 +13,17 @@ public class Jeu {
         // Ajouter les joueurs à la matrice
         Matrice.ajouterJoueur(matrice, joueur1);
         Matrice.ajouterJoueur(matrice, joueur2);
+
+        // Choisir aléatoirement le joueur qui commence
+        Random random = new Random();
+        boolean joueur1Commence = random.nextBoolean();
+
+        // Afficher le message du joueur qui commence
+        if (joueur1Commence) {
+            System.out.println("Le joueur 1 commence !");
+        } else {
+            System.out.println("Le joueur 2 commence !");
+        }
 
         // Afficher la matrice avec les joueurs depuis la classe AffichageMatrice
         Matrice.affichageMatrice(matrice);
