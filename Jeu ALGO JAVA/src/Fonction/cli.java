862b73687185035f9ef3b810b6fb57490cf59177
Wolphen <enzo.grignon@edu.esiee.fr>
2024-02-06 16:22:40 +0100

    Assemblement de l'affichage et du menu

---
+++ Jeu ALGO JAVA/src/Fonction/cli.java
@@ -0,0 +1,77 @@
+package Fonction;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class cli {
+    public static void menu() {
+        // Efface la console pour une nouvelle interaction utilisateur
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        // Initialise un scanner pour lire l'entrée utilisateur
+        Scanner scanner = new Scanner(System.in);
+
+        // Affiche le message de bienvenue et le menu
+        System.out.println("\n    BIENVENUE SUR CHESS DESTRUCT\r\n\n");
+        System.out.println("1 - Commencer une partie    \n2 - Règles  \n3 - Scores   \n4 - Quitter\r\n");
+
+        try {
+            // Lit le choix de l'utilisateur pour le menu
+            int response = scanner.nextInt();
+
+            // Utilise une instruction switch pour traiter différentes options du menu
+            switch (response) {
+                case 1:
+                    // Option 1: Affiche "Jeu"
+                    System.out.println("Jeu");
+                    affichageMatrice.affichageMatrice(creationMatrice.creationMatrice()); // fonction qui crée et affiche la matrice
+                    break;
+                case 2:
+                    // Option 2: Affiche les règles, puis attend pendant 5 secondes
+                    System.out.println("Règles : ");
+                    System.out.println("Pendant son tour, un joueur peut déplacer son pion d’une case (verticalement ou horizontalement), puis il détruit une case du plateau.\n" +
+                            "Le dernier joueur pouvant encore se déplacer gagne.\n" +
+                            "Contraintes :\n" +
+                            "- Un joueur ne peut pas détruire une case occupée.\n" +
+                            "- Un joueur ne peut pas occuper une case détruite ou une case déjà occupée.\n" +"- Un joueur bloqué pendant un tour est déclaré perdant.");
+                    try {
+                        Thread.sleep(2000); // Attendre pendant 2 secondes
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    menu(); // Appelle la fonction menu
+                    break;
+                case 3:
+                    // Option 3: Affiche "Scores"
+                    System.out.println("Scores");
+                    break;
+                case 4:
+                    // Option 4: Affiche "Quitter" et quitte le programme
+                    System.out.println("Quitter");
+                    System.exit(0);
+                default:
+                    // Cas par défaut pour une entrée invalide
+                    System.out.println("Rentrez un chiffre entier en 1 et 4");
+
+                    try {
+                        Thread.sleep(1000); // Attendre pendant 1 seconde
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    menu(); // Appelle la fonction menu
+                    break;
+            }
+        } catch (InputMismatchException e) {
+            // Gère l'exception si l'utilisateur entre une valeur non entière
+            System.out.println("Rentrez un CHIFFRE entier en 1 et 4");
+            try {
+                Thread.sleep(1000); // Attendre pendant 1 seconde
+            } catch (InterruptedException r) {
+                throw new RuntimeException(r);
+            }
+            menu();
+        }
+    }
+
+}
