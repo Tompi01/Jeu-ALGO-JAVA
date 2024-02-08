01012464638fbe72be6a79a7800f67ded506c107
Wolphen <enzo.grignon@edu.esiee.fr>
2024-02-08 09:03:00 +0100

    deplacement done

--- Jeu ALGO JAVA/src/Fonction/deplacement.java
+++ Jeu ALGO JAVA/src/Fonction/deplacement.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class deplacement{
 
     /*
-    fonction de déplacement du joueur
+    * fonction de déplacement du joueur
     * Joueur qui joue : ID | des position X : Y | Condition : Si la case est != 0 | passe le tour au joueur suivant |
     * scanner string = Z : monte - 1 colonne | S + 1 colonne | Q = - 1 ligne | D = + 1 ligne ou bah tu remet ton
     * input chacal.
@@ -17,7 +17,7 @@ public class deplacement{
     public static Joueur deplacementDuJoueur(Joueur joueur, int[][] matrice){
         Scanner scanner = new Scanner(System.in);
         try {
-            int positionLJoueur=joueur.getPositionX(); // stock x et y en ligne et colonne
+            int positionLJoueur=joueur.getPositionX(); // stock y et x en ligne et colonne
             int positionCJoueur = joueur.getPositionY();
             System.out.println("Rentrez une lettre entre : Z - Haut | S - Bas | Q - Gauche | D - Droite&");
             System.out.println("Le joueur " + (joueur.getId()-1) + " commence !");
@@ -27,7 +27,7 @@ public class deplacement{
                     if (matrice[positionLJoueur ][positionCJoueur - 1] != 0) { // if supposer check si au dessus de lui la case est = à 0
 
                         System.out.println("impossible, recommencez");
-                        deplacementDuJoueur(joueur, matrice);
+                        deplacementDuJoueur(joueur, matrice); // réexecute la fonction en gardant le joueur actuelle
                         break;
                 }
                     else{
@@ -38,15 +38,12 @@ public class deplacement{
                         Matrice.affichageMatrice(matrice);
                         break;
                     }
-
                 case "Q":// déplacement du joueur à gauche
                     if (matrice[positionLJoueur -1][positionCJoueur] != 0) { // if supposer check si au dessus de lui la case est = à 0
-
                         System.out.println("impossible, recommencez");
-                        deplacementDuJoueur(joueur, matrice);
+                        deplacementDuJoueur(joueur, matrice); // réexecute la fonction en gardant le joueur actuelle
                         break;
                     }
-
                     else{
                         matrice[positionLJoueur-1][positionCJoueur]=joueur.getId(); //regarde dans la matrice la position du joueur actuel
                         matrice[positionLJoueur][positionCJoueur]=0; //ancienne position ou on réinitialise la case à son états de base (jouable)
@@ -54,24 +51,23 @@ public class deplacement{
                         Matrice.affichageMatrice(matrice);
                         break;
                     }
-
                 case "S":// déplacement du joueur en bas
                     if (matrice[positionLJoueur][positionCJoueur +1] != 0) { // if supposer check si au dessus de lui la case est = à 0
-
-                        System.out.println("impossible, recommencez");
-                        deplacementDuJoueur(joueur, matrice);
+                        System.out.println("impossible, recommence");
+                        deplacementDuJoueur(joueur, matrice); // réexecute la fonction en gardant le joueur actuelle
                         break;
                     }
                     else {
                         matrice[positionLJoueur][positionCJoueur + 1] = joueur.getId();
                         matrice[positionLJoueur][positionCJoueur] = 0;
                         joueur.setPosition(positionLJoueur, positionCJoueur + 1);
-                        Matrice.affichageMatrice(matrice);
+                        Matrice.affichageMatrice(matrice); //
                         break;
                     }
 
                 case "D":// déplacement du joueur à droite
                     if (matrice[positionLJoueur +1][positionCJoueur] != 0) { // if supposer check si au dessus de lui la case est = à 0
+
 
                         System.out.println("impossible, recommencez");
                         deplacementDuJoueur(joueur, matrice);
@@ -87,22 +83,20 @@ public class deplacement{
 
                 default:
                     System.out.println("Rentrez une lettre entre Z - Q - S - D");
-                    deplacementDuJoueur(joueur, matrice);
+                    deplacementDuJoueur(joueur, matrice); // réexecute la fonction en gardant le joueur actuelle
             }
-
             return joueur;
         }catch (InputMismatchException e){
             // Gère l'exception si l'utilisateur entre une valeur non entière
             System.out.println("Rentrez une lettre entre Z - Q - S - D");
-            deplacementDuJoueur(joueur, matrice);
+            deplacementDuJoueur(joueur, matrice); // réexecute la fonction en gardant le joueur actuelle
             try {
                 Thread.sleep(1000); // Attendre pendant 1 seconde
             } catch (InterruptedException r) {
                 throw new RuntimeException(r);
             }
-            deplacementDuJoueur(joueur, matrice);
+            deplacementDuJoueur(joueur, matrice); // réexecute la fonction en gardant le joueur actuelle
         };
-
         return joueur;
     }
 }
